76fc766590401e3bbdc7c797b43cbb1ec11fd62d
Eduardo Guillen <eguillen2035@gmail.com>
2020-08-14 16:14:25 -0500

    Succesful  running on port fixed paths and routes

---
+++ routes/routes_api.js
@@ -0,0 +1,78 @@
+// DEPENDENCIES
+const router = require('express').Router();
+const Workout = require('../models/workout');
+
+// GET/READ WORKOUT
+router.get('/api/workouts', (req, res) => {
+	Workout.find({})
+		// add index to sort by ascending days
+		.sort({ day: 1 })
+		.then((data) => {
+			res.json(data);
+			console.log(data);
+		})
+		// if there is an error, display error
+		.catch((err) => {
+			res.status(400).json(err);
+			console.log(err);
+		});
+});
+
+// CREATE WORKOUT
+router.post('/api/workouts', ({ body }, res) => {
+	Workout.create(body)
+		.then((data) => {
+			res.json(data);
+			console.log(data);
+		})
+		// if there is an error, display error
+		.catch((err) => {
+			res.status(400).json(err);
+			console.log(err);
+		});
+});
+
+// UPDATE WORKOUT BY ID
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+	Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } }, { new: true, runValidators: true })
+		.then((data) => {
+			res.json(data);
+			console.log(data);
+		})
+		// if there is an error, display error
+		.catch((err) => {
+			res.status(400).json(err);
+			console.log(err);
+		});
+});
+
+// GET/READ STATS
+router.get('/api/workouts/range', (req, res) => {
+	Workout.find()
+		.then((data) => {
+			res.json(data);
+			console.log(data);
+		})
+		// if there is an error, display error
+		.catch((err) => {
+			res.status(400).json(err);
+			console.log(err);
+		});
+});
+
+// POST STATS
+router.post('/api/workouts/range', (req, res) => {
+	Workout.create({})
+		.then((data) => {
+			res.json(data);
+			console.log(data);
+		})
+		// if there is an error, display error
+		.catch((err) => {
+			res.status(400).json(err);
+			console.log(err);
+		});
+});
+
+// EXPORTS
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 // PORT
 const PORT = process.env.PORT || 4000;
@@ -9,18 +9,25 @@ const PORT = process.env.PORT || 4000;
 //Middleware -------
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"))
+app.use(express.static('./Develop/public'));
 
 // ROUTES
-app.use(require("./routes/routes_api.js"));
-app.use(require("./routes/routes_html.js"));
+app.use(require('./routes/routes_api.js'));
+app.use(require('./routes/routes_html.js'));
 
 // START SERVER
 app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}!`);
+	console.log(`App is running on port ${PORT}!`);
 });
+
+
+//Routes API, Get ROUTES add APIS/ WORKOUT/RANGE
+
+//CREATE ROUTES FOR THAT SO i CAN SEE WORKOUTS ON THE STATS 
+
+//sHOUDL BE SET AFTER THAT
